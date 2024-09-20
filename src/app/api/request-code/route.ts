dee667ddf9d97b95fa7d49d1287eac40f7ea50a9
owell andry <owellandry10@gmail.com>
2024-09-19 22:36:27 -0500

    Update changes

--- src/app/api/request-code/route.ts
+++ src/app/api/request-code/route.ts
@@ -27,25 +27,24 @@ export async function POST(request: Request) {
     const mailOptions = {
         from: 'ChiguiStudio <studio.cubysoft@gmail.com>',
         to: email,
-        replyTo: 'soporte@chiguistudio.com',
-        cc: 'admin@chiguistudio.com',
-        bcc: 'manager@chiguistudio.com',
+        replyTo: 'studio.cubysoft@gmail.com',
+        cc: 'studio.cubysoft@gmail.com',
+        bcc: 'studio.cubysoft@gmail.com',
         subject: 'Código de Verificación',
         html: `
-        <body style="font-family: Arial, sans-serif; background-color: #f0f2f5; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh;">
-            <div style="max-width: 600px; margin: auto; padding: 0; border-radius: 8px; background: #ffffff; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
-                
-                <!-- Cabecera con logo -->
-                <div style="background-color: #007bff; color: white; padding: 20px; text-align: center;">
-                    <img src="cid:logo@chiguistudio" alt="ChiguiStudio Logo" style="border-radius: 50%; width: 80px; height: 80px;">
-                    <h1 style="font-size: 26px; font-weight: bold; margin-top: 10px;">ChiguiStudio</h1>
+        <body style="font-family: Arial, sans-serif; background-color: #f0f2f5; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw;">
+            <div style="margin: auto; padding: 0; border-radius: 8px; background: #ffffff; box-shadow: 0 4px 8px rgba(0,0,0,0.1); width: 100%; max-width: 600px;">
+
+                <!-- Cabecera con logo/banner -->
+                <div style="color: white; text-align: center;">
+                    <img src="cid:logo@chiguistudio" alt="CubySoft Logo" style="width: 100%; height: auto; border-radius: 0;" draggable="false">
                 </div>
                 
                 <!-- Contenido principal -->
                 <div style="padding: 20px; text-align: center;">
-                    <h2 style="font-size: 24px; font-weight: bold; color: #333; margin-bottom: 20px;">¡Hola!</h2>
+                    <h2 style="font-size: 24px; font-weight: bold; color: #333; margin-bottom: 20px;">¡Te damos la bienvenida!</h2>
                     <p style="font-size: 16px; color: #555; margin: 10px 0;">Tu código de verificación es:</p>
-                    <div style="font-size: 40px; color: #ffffff; background-color: #007bff; padding: 15px; border-radius: 5px; display: inline-block; font-weight: bold; margin: 20px 0; letter-spacing: 3px;">
+                    <div style="font-size: 40px; color: #333; padding: 0px 30px; border-radius: 5px; display: inline-block; font-weight: bold; margin: 20px 0; letter-spacing: 3px; border: 1px #d7d7d7 solid; box-shadow: 1px 5px 5px 0px gray;">
                         ${verificationCode}
                     </div>
                     <p style="font-size: 16px; color: #e74c3c; margin: 20px 0;">Este código es válido por <strong>5 minutos.</strong><br> Si no solicitaste este código, por favor ignora este mensaje.</p>
@@ -63,14 +62,9 @@ export async function POST(request: Request) {
         `,
         attachments: [
             {
-                filename: 'logo.png',
-                path: path.resolve('assets/logo.png'), // Ruta absoluta al archivo de logo
+                filename: 'BannerUp.png',
+                path: path.resolve('assets/BannerUp.png'),
                 cid: 'logo@chiguistudio',
-            },
-            {
-                filename: 'guia-usuario.pdf',
-                path: path.resolve('assets/guia-usuario.pdf'), // Ruta absoluta al archivo PDF
-                contentType: 'application/pdf',
             }
         ],
     };
